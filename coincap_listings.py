1b0dfbdec34055cd1a353636d05905bb95b7f6e0
lawrencemulders <80403668+lawrencemulders@users.noreply.github.com>
2022-11-08 22:47:35 +0100

    Update coincap_listings.py

--- coincap_listings.py
+++ coincap_listings.py
@@ -6,7 +6,7 @@ import json
 local_currency = 'USD'
 local_symbol = '$'
 
-api_key = 'personalAPIkey' // removed as each key is personal
+api_key = 'enter_api_key' // api key removed
 headers = {'X-CMC_PRO_API_KEY': api_key}
 
 base_url = 'https://pro-api.coinmarketcap.com'
