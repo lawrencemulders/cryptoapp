fcf7ee03db47cd9c77fe5e5458d635e38b188045
lawrencemulders <lawrencemulders7@hotmail.com>
2022-12-11 23:06:58 +0100

    First commit to defining a function

--- coincap_listings.py
+++ coincap_listings.py
@@ -5,41 +5,41 @@ import json
 
 def cryptolistings():
 
-local_currency = 'USD'
-local_symbol = '$'
+    local_currency = 'USD'
+    local_symbol = '$'
 
-api_key = 'enter_api_key' # api key removed
-headers = {'X-CMC_PRO_API_KEY': api_key}
+    api_key = 'enter_api_key' # api key removed
+    headers = {'X-CMC_PRO_API_KEY': api_key}
 
-base_url = 'https://pro-api.coinmarketcap.com'
+    base_url = 'https://pro-api.coinmarketcap.com'
 
-global_url = base_url + '/v1/cryptocurrency/listings/latest?convert=' + local_currency
+    global_url = base_url + '/v1/cryptocurrency/listings/latest?convert=' + local_currency
 
-request = requests.get(global_url, headers=headers)
-results = request.json()
+    request = requests.get(global_url, headers=headers)
+    results = request.json()
 
 # print(json.dumps(results, sort_keys=True, indent=4))
 
-data = results["data"]
+    data = results["data"]
 # Loop through each cryptocurrency
-for currency in data:
-    name = currency['name']
-    symbol = currency['symbol']
+    for currency in data:
+        name = currency['name']
+        symbol = currency['symbol']
 
-    price = currency['quote'][local_currency]['price']
-    percent_change_24h = currency['quote'][local_currency]['percent_change_24h']
-    market_cap = currency['quote'][local_currency]['market_cap']
+        price = currency['quote'][local_currency]['price']
+        percent_change_24h = currency['quote'][local_currency]['percent_change_24h']
+        market_cap = currency['quote'][local_currency]['market_cap']
 
-    price = round(price, 2)
-    percent_change_24h = round(percent_change_24h, 2)
-    market_cap = round(market_cap, 2)
+        price = round(price, 2)
+        percent_change_24h = round(percent_change_24h, 2)
+        market_cap = round(market_cap, 2)
 
-    price_string = local_symbol + '{:,}'.format(price)
-    percent_change_24h_string = local_symbol + '{:,}'.format(percent_change_24h)
-    market_cap_string = local_symbol + '{:,}'.format(market_cap)
+        price_string = local_symbol + '{:,}'.format(price)
+        percent_change_24h_string = local_symbol + '{:,}'.format(percent_change_24h)
+        market_cap_string = local_symbol + '{:,}'.format(market_cap)
 
-    print(name + ' (' + symbol + ') ')
-    print('Price: ' + price_string)
-    print('24h Change: ' + percent_change_24h_string)
-    print('Market Cap: ' + market_cap_string)
-    print()
+        print(name + ' (' + symbol + ') ')
+        print('Price: ' + price_string)
+        print('24h Change: ' + percent_change_24h_string)
+        print('Market Cap: ' + market_cap_string)
+        print()
